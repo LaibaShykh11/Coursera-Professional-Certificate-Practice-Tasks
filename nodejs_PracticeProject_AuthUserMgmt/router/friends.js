0a34b83ce5aeed0246921d087e5a1b68ee11fdde
LaibaShykh11 <134623689+LaibaShykh11@users.noreply.github.com>
2025-11-06 00:09:40 +0500

    Add files via upload

---
+++ nodejs_PracticeProject_AuthUserMgmt/router/friends.js
@@ -0,0 +1,76 @@
+const express = require('express');
+
+const router = express.Router();
+
+let friends = {
+    "johnsmith@gamil.com": {"firstName": "John","lastName": "Doe","DOB":"22-12-1990"},
+    "annasmith@gamil.com":{"firstName": "Anna","lastName": "smith","DOB":"02-07-1983"},
+    "peterjones@gamil.com":{"firstName": "Peter","lastName": "Jones","DOB":"21-03-1989"}
+};
+
+
+// GET request: Retrieve all friends
+router.get("/",(req,res)=>{
+
+  res.send(JSON.stringify(friends,null,4))//This line is to be replaced with actual return value
+});
+
+// GET by specific ID request: Retrieve a single friend with email ID
+router.get("/:email",(req,res)=>{
+   const email = req.params.email
+   res.send(friends[email])
+ 
+});
+
+
+// POST request: Add a new friend
+router.post("/",(req,res)=>{
+  if(req.body.email) {
+    friends[req.body.email] = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,  
+      DOB: req.body.DOB
+  }
+}
+res.send('The user'+('')+(req.body.firstName)+'has been added')
+});
+
+
+// PUT request: Update the details of a friend with email id
+router.put("/:email", (req, res) => {
+  router.put("/:email", function(req, res) {
+    // Extract email parameter from request URL
+    const email = req.params.email;
+    let friend = friends[email];  // Retrieve friend object associated with email
+
+    if (friend) {  // Check if friend exists
+        let DOB = req.body.DOB;
+        // Add similarly for firstName
+        // Add similarly for lastName
+
+        // Update DOB if provided in request body
+        if (DOB) {
+            friend["DOB"] = DOB;
+        }
+        // Add similarly for firstName
+        // Add similarly for lastName
+
+        friends[email] = friend;  // Update friend details in 'friends' object
+        res.send(`Friend with the email ${email} updated.`);
+    } else {
+        // Respond if friend with specified email is not found
+        res.send("Unable to find friend!");
+    }
+});
+});
+
+
+// DELETE request: Delete a friend by email id
+router.delete("/:email", (req, res) => {
+  const email = req.params.email;
+  if (friends[email]) {
+    delete friends[email];  }
+    res.send(`Friend with the email ${email} has been deleted.`);
+});
+
+module.exports=router;
